e4515ad63de162a0b1a0f82e73bf4e5356032d08
yamdev07 <yamdyamd080@gmail.com>
2025-12-02 18:09:42 +0100

    update profile to show more information

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,29 +1,40 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:profile.update-profile-information-form />
-                </div>
-            </div>
+@section('title', 'Profile')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:profile.update-password-form />
-                </div>
-            </div>
+@section('styles')
+    @livewireStyles
+@endsection
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:profile.delete-user-form />
-                </div>
+@section('content')
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+        {{-- Mettre à jour les informations du profil --}}
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                <livewire:profile.update-profile-information-form />
             </div>
         </div>
+
+        {{-- Mettre à jour le mot de passe --}}
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                <livewire:profile.update-password-form />
+            </div>
+        </div>
+
+        {{-- Supprimer le compte --}}
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                <livewire:profile.delete-user-form />
+            </div>
+        </div>
+
     </div>
-</x-app-layout>
+</div>
+@endsection
+
+@section('scripts')
+    @livewireScripts
+@endsection
